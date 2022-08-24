d970e8f525929a49adae589e450eb19b4aa2afdb
Darijush Prokopovich <darijus.pc@gmail.com>
2022-08-24 14:36:52 +0300

    coposer done

--- 08-24/book.php
+++ 08-24/book.php
@@ -2,7 +2,7 @@
 
 use Petro\Read;
 
-// require __DIR__ . '/01/Title.php';
+require __DIR__ . '/vendor/autoload.php';
 // require __DIR__ . '/01/Read.php';
 // require __DIR__ . '/02/Read.php';
 spl_autoload_register(function ($class) {
