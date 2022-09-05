180c3921b92bdee97f3a202bc4d8123dee01abe2
Darijush Prokopovich <darijus.pc@gmail.com>
2022-09-05 12:39:36 +0300

    react partly

--- 08-29/App/App.php
+++ 08-29/App/App.php
@@ -6,6 +6,7 @@ use App\Controllers\HomeController as HC;
 use App\Controllers\AnimalController as A;
 use App\Controllers\LoginController as L;
 use App\Controllers\ApiController as Api;
+use App\Controllers\ReactController as Re;
 use App\Middleware\Auth;
 
 class App
@@ -22,7 +23,7 @@ class App
         $url = explode('/', $url);
         array_shift($url);
         $method = $_SERVER['REQUEST_METHOD'];
-        if(!Auth::authorize($url)){
+        if (!Auth::authorize($url)) {
             return self::redirect('login');
         }
         if ($method == 'GET' && count($url) == 1 && $url[0] == '') {
@@ -33,26 +34,27 @@ class App
             return ((new A)->store());
         } elseif ($method == 'GET' && count($url) == 1 && $url[0] == 'animals') {
             return ((new A)->list());
-        }elseif ($method == 'GET' && count($url) == 3 && $url[0] == 'animals' && $url[1] == 'edit' ) {
+        } elseif ($method == 'GET' && count($url) == 3 && $url[0] == 'animals' && $url[1] == 'edit') {
             return ((new A)->edit($url[2]));
-        }elseif ($method == 'POST' && count($url) == 3 && $url[0] == 'animals' && $url[1] == 'update' ) {
+        } elseif ($method == 'POST' && count($url) == 3 && $url[0] == 'animals' && $url[1] == 'update') {
             return ((new A)->update($url[2]));
-        }elseif ($method == 'POST' && count($url) == 3 && $url[0] == 'animals' && $url[1] == 'delete' ) {
+        } elseif ($method == 'POST' && count($url) == 3 && $url[0] == 'animals' && $url[1] == 'delete') {
             return ((new A)->delete($url[2]));
-        }elseif (($method == 'GET' && count($url) == 1 && $url[0] == 'login' )) {
-            if(Auth::isLogged()){
+        } elseif (($method == 'GET' && count($url) == 1 && $url[0] == 'login')) {
+            if (Auth::isLogged()) {
                 return self::redirect('');
             }
             return ((new L)->login());
-        }elseif (($method == 'POST' && count($url) == 1 && $url[0] == 'login' )) {
+        } elseif (($method == 'POST' && count($url) == 1 && $url[0] == 'login')) {
             return ((new L)->doLogin());
-        }elseif (($method == 'POST' && count($url) == 1 && $url[0] == 'logout' )) {
+        } elseif (($method == 'POST' && count($url) == 1 && $url[0] == 'logout')) {
             return ((new L)->logout());
-        }elseif ($method == 'GET' && count($url) == 2 && $url[0] == 'api' && $url[1] == 'go' ) {
+        } elseif ($method == 'GET' && count($url) == 2 && $url[0] == 'api' && $url[1] == 'go') {
             return ((new Api)->show());
-        }
-        elseif ($method == 'POST' && count($url) == 2 && $url[0] == 'api' && $url[1] == 'go' ) {
+        } elseif ($method == 'POST' && count($url) == 2 && $url[0] == 'api' && $url[1] == 'go') {
             return ((new Api)->doApi());
+        }elseif ($method == 'GET' && count($url) == 2 && $url[0] == 'react' && $url[1] == 'list') {
+            return ((new Re)->list());
         }
     }
     static public function view($name, $data = [])
@@ -63,5 +65,10 @@ class App
     static public function redirect($where)
     {
         header('Location:' . URL . $where);
+    }
+    static public function json($data)
+    {   
+        header("Content-Type: application/json");
+        echo json_decode($data);
     }
 }
--- zoo/src/App.jsx
+++ zoo/src/App.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.scss';
+import Create from './Components/Create';
+import List from './Components/List';
+import axios from 'axios';
 
 function App() {
+  const [animals, setAnimals] = useState(null);
+  useEffect(() => {
+    axios.get('http://animals.zoo/react/list')
+      .then(res => setAnimals(res.data));
+  }, []);
   return (
-    <div className="App">
-      <h1>Ja</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-5">
+          <Create />
+        </div>
+        <div className="col-7">
+          <List list={animals} />
+        </div>
+      </div>
     </div>
   );
 }
