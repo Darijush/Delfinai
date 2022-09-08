9485bde62c7a8d0c43a2633cf150665cd5ad61b0
Darijush Prokopovich <darijus.pc@gmail.com>
2022-09-08 13:14:26 +0300

    laravel installed

---
+++ darijush-laravel/app/Http/Controllers/NiceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NiceController extends Controller
+{
+    public function fun($duok, $kuku){
+        return $kuku.' FUN: '.$duok;
+    }
+}
