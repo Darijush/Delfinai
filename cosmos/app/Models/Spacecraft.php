4a9a88aa3c8692ac7aeeee8788bb5e7df8aaf108
Darijush Prokopovich <darijus.pc@gmail.com>
2022-09-16 08:51:55 +0300

    cosmos prepartion for db done

---
+++ cosmos/app/Models/Spacecraft.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Spacecraft extends Model
+{
+    use HasFactory;
+}
