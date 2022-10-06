95e6ea8036cb17f856c5493dd708a2a024885dc9
Darijush Prokopovich <darijus.pc@gmail.com>
2022-10-06 14:30:29 +0300

    df2 init commit

---
+++ df2/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as F;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $time = Carbon::now();
+        $faker = F::create('lt_LT');
+        DB::table('users')->insert([
+            'name' => 'bebras',
+            'email' => 'bebras@gmail.com',
+            'password' => Hash::make('123'),
+            'created_at' => $time->addSeconds(1),
+            'updated_at' => $time,
+            'role'=> 1
+        ]);
+        DB::table('users')->insert([
+            'name' => 'briedis',
+            'email' => 'briedis@gmail.com',
+            'password' => Hash::make('123'),
+            'created_at' => $time->addSeconds(1),
+            'updated_at' => $time,
+            'role' => 10
+        ]);
+
+        foreach(['Cobra', 'Jaguar', 'Sharks', 'Cats and Dogs', 'Coporob', 'Funny movie', 'Lara and Tombs', 'Jasa and Vue'] as $movie){
+            DB::table('movies')->insert([
+                'title' => $movie,
+                'price' => rand(100,1000)/100,
+                'created_at' => $time->addSeconds(1),
+                'updated_at' => $time
+            ]);
+        }
+        foreach (range(1,22) as $_){
+            DB::table('comments')->insert([
+                'movie_id' => rand(1,8),
+                'post' => $faker->paragraph(rand(1, 10)),
+                'created_at' => $time->addSeconds(1),
+                'updated_at' => $time
+            ]);
+        }
+    }
+}
---
+++ df2/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController as C;
+use App\Http\Controllers\MovieController as M;
+use App\Http\Controllers\HomeController as H;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+Route::get('/', [H::class, 'homeList'])->name('home_list')->middleware('gate:home');
+Route::put('/rate/{movie}', [H::class, 'rate'])->name('rate')->middleware('gate:users');
+
+
+Route::prefix('movie')->name('m_')->group(function () {
+    Route::get('/', [M::class, 'index'])->name('index')->middleware('gate:users');
+    Route::get('/create', [M::class, 'create'])->name('create')->middleware('gate:admin');
+    Route::post('/create', [M::class, 'store'])->name('store')->middleware('gate:admin');
+    Route::get('/show/{movie}', [M::class, 'show'])->name('show')->middleware('gate:users');
+    Route::delete('/delete/{movie}', [M::class, 'destroy'])->name('delete')->middleware('gate:admin');
+    Route::get('/edit/{movie}', [M::class, 'edit'])->name('edit')->middleware('gate:admin');
+    Route::put('/edit/{movie}', [M::class, 'update'])->name('update')->middleware('gate:admin');
+});
