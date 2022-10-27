837c81e162bd5f1e9f1cfbe17b630474b8d46c92
Darijush Prokopovich <darijus.pc@gmail.com>
2022-10-27 11:15:55 +0300

    automatic slider df1

--- df1/resources/js/app.js
+++ df1/resources/js/app.js
@@ -7,7 +7,21 @@
 import './bootstrap';
 import { createApp } from 'vue';
 
-
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
 
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
